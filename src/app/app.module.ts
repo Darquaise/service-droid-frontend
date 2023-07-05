0fe1c2c2ddc4bb406057598683417e4f7f6ad80b
Darquaise <kofi.diering@icloud.com>
2023-07-05 16:37:07 +0200

    added package tailwind added package primeng added package primefaces reformatted some files removed testing file of app.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+/* Angular Modules */
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientModule} from "@angular/common/http";
+import {RouterModule, Routes} from "@angular/router";
+import {NgOptimizedImage} from "@angular/common";
+import {AppRoutingModule} from './app-routing.module';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+/* PrimeNG Modules */
+// soon to be added
+
+/* Components */
+import {AppComponent} from './app.component';
+
+/* Services */
+// soon to be added
+
+const routes: Routes = [
+    {path: '', component: AppComponent}
+]
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        RouterModule.forRoot(routes),
+        BrowserModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        HttpClientModule,
+        NgOptimizedImage
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
